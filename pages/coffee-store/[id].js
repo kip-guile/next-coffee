d353478c6b60958c90eb6ad16ec094ccaf0e6d2e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2022-07-06 16:38:28 +0100

    feat: dynamic page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,17 +1,111 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
+import cls from 'classnames'
 
-const CoffeeStore = ({}) => {
+import styles from '../../styles/coffee-store.module.css'
+import coffeeStoresData from '../../data/coffee-stores.json'
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (el) => el.id.toString() === params.id
+      ),
+    }, // will be passed to the page component as props
+  }
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id.toString(),
+      },
+    }
+  })
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+const CoffeeStore = ({
+  coffeeStore: { address, neighbourhood, name, imgUrl },
+}) => {
+  console.log(address, neighbourhood, name, imgUrl)
   const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
   return (
-    <div>
-      Coffee Store {router.query.id}
-      <Link href='/'>
-        <a>Back to home</a>
-      </Link>
-      <Link href='/coffee-store/2'>
-        <a>Back to home</a>
-      </Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+        <meta name='description' content={`${name} coffee store`} />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'>
+              <a>← Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+
+        <div className={cls('glass', styles.col2)}>
+          {address && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src='/static/icons/places.svg'
+                width='24'
+                height='24'
+                alt='places icon'
+              />
+              <p className={styles.text}>{address}</p>
+            </div>
+          )}
+          {neighbourhood && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src='/static/icons/nearMe.svg'
+                width='24'
+                height='24'
+                alt='near me icon'
+              />
+              <p className={styles.text}>{neighbourhood}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/static/icons/star.svg'
+              width='24'
+              height='24'
+              alt='star icon'
+            />
+            <p className={styles.text}>5</p>
+          </div>
+
+          <button className={styles.upvoteButton} onClick={() => {}}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/banner'
+import Card from '../components/card'
 import styles from '../styles/Home.module.css'
+import coffeeStoresData from '../data/coffee-stores.json'
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return {
+    props: {
+      coffeeStores: coffeeStoresData,
+    }, // will be passed to the page component as props
+  }
+}
+
+export default function Home({ coffeeStores }) {
   const handleOnBannerClick = () => {
     console.log('click')
   }
@@ -22,6 +32,30 @@ export default function Home() {
         />
         <div className={styles.heroImage}>
           <Image alt='hero' src='/static/hero.png' width={400} height={400} />
+        </div>
+
+        <div className={styles.sectionWrapper}>
+          {coffeeStores.length > 0 && (
+            <>
+              <h2 className={styles.heading2}>Toronto stores</h2>
+              <div className={styles.cardLayout}>
+                {coffeeStores.map((coffeeStore) => {
+                  return (
+                    <Card
+                      key={coffeeStore.id}
+                      name={coffeeStore.name}
+                      imgUrl={
+                        coffeeStore.imgUrl ||
+                        'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                      }
+                      href={`/coffee-store/${coffeeStore.id}`}
+                      className={styles.card}
+                    />
+                  )
+                })}
+              </div>
+            </>
+          )}
         </div>
       </main>
     </div>
